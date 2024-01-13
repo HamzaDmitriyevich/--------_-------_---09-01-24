286ad5fc1540bcf67bc36ef8f2157d88fe258a93
HamzaDmitriyevich <krou1937@gmail.com>
2024-01-13 19:22:25 +0300

    Сделал 2 новые функции все работает

--- functions.js
+++ functions.js
@@ -8,3 +8,24 @@ function date (){
     let date = new Date();
     console.log(date);
 }
+
+function prazdnik (Ramadan){
+    for(var i = 0; i<5; i++) {
+    console.log(Ramadan);
+    }
+}
+
+function odnigrupniki(){
+    var a = ['Сергей', 'Олег', 'Павел', 'Дарья', 'Уулжан']
+    for(var i =0; i < a.length; i++){
+        console.log(a[i]);
+    }
+}
+
+
+
+
+
+
+
+
